fe5f239526259677a46a2c810a8d6f2b9b81b469
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-04-27 22:56:31 +0530

    chore(web): Added constants file

---
+++ apps/web/src/lib/constants/index.ts
@@ -0,0 +1,10 @@
+const navigationItems = [
+	{ name: "Home", href: "/" },
+	{ name: "Library", href: "/library" },
+	{ name: "Playlists", href: "/playlists" },
+	{ name: "Player", href: "/player" },
+	{ name: "Upload", href: "/upload" },
+	{ name: "Settings", href: "/settings" },
+];
+
+export { navigationItems };
