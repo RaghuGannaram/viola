dbae08adc2366d07c5f11d27f860aaa393daf63d
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-04-27 22:56:05 +0530

    chore(web): Added types file

---
+++ apps/web/src/lib/types/index.ts
@@ -0,0 +1,11 @@
+export interface ITrack {
+	id: string;
+	name: string;
+	blob: Blob;
+	url: string;
+	title?: string;
+	artist?: string;
+	album?: string;
+	coverImage?: string;
+	lyrics?: string;
+}
