0048bff255bdc61ebeeb71e882813852aa4430b3
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-04-27 22:57:35 +0530

    feat(web): Added Icons component

---
+++ apps/web/src/lib/components/Icon/index.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import "iconify-icon";
+
+	const {
+		name,
+		size = 24,
+		className = "",
+	} = $props<{
+		name: string;
+		size?: number;
+		className?: string;
+	}>();
+</script>
+
+<iconify-icon icon={name} width={size} height={size} class={className}></iconify-icon>
