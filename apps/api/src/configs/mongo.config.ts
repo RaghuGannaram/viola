b2d7c2ddbb099f97f924447dacfee7ceded9dfb5
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-05-13 14:25:46 +0530

    feat(api): Configured mongodb

---
+++ apps/api/src/configs/mongo.config.ts
@@ -0,0 +1,84 @@
+import chalk from "chalk";
+import mongoose, { type Connection } from "mongoose";
+import { type ConnectOptions } from "mongoose";
+import logger from "@src/configs/logger.config";
+import { getMongoUrl, getCurrentEnv } from "@src/utils/env-info";
+
+type Config = Record<ReturnType<typeof getCurrentEnv>, ConnectOptions>;
+
+const mongoURL = getMongoUrl();
+const currentEnv = getCurrentEnv();
+
+const dbConfig: Config = {
+	development: {
+		dbName: "developmentDB",
+		bufferCommands: true,
+		family: 4,
+		maxPoolSize: 100,
+		socketTimeoutMS: 30000,
+		connectTimeoutMS: 30000,
+		serverSelectionTimeoutMS: 5000,
+	},
+	production: {
+		dbName: "productionDB",
+		autoIndex: false,
+		autoCreate: false,
+		bufferCommands: false,
+		family: 4,
+		maxPoolSize: 100,
+		socketTimeoutMS: 30000,
+		connectTimeoutMS: 30000,
+		serverSelectionTimeoutMS: 30000,
+	},
+};
+
+const mongoOptions = dbConfig[currentEnv];
+
+mongoose.set("debug", currentEnv === "development");
+mongoose.connect(mongoURL, mongoOptions);
+
+const db: Connection = mongoose.connection;
+
+db.on("connecting", () => {
+	logger.info("mongodb server: connecting...");
+});
+
+db.on("connected", () => {
+	const { name, host, port, models } = db;
+
+	logger.info("mongodb server: successful...🍃");
+	logger.debug(`mongodb server: host: ${chalk.magenta("%s")}:${chalk.magenta("%s")}`, host, port);
+	logger.debug(`mongodb server: name: ${chalk.magenta("%s")}, models: ${chalk.grey("%o")}`, name, models);
+});
+
+db.on("open", () => {
+	logger.info("mongodb server: open...");
+});
+
+db.on("disconnecting", () => {
+	logger.info("mongodb server: disconnecting...");
+});
+
+db.on("disconnected", () => {
+	logger.error("mongodb server: disconnected...");
+});
+
+db.on("close", () => {
+	logger.error("mongodb server: closed...");
+});
+
+db.on("reconnected", () => {
+	logger.info("mongodb server: reconnected...");
+});
+
+db.on("error", () => {
+	logger.error("mongodb server: Unable to connect to Database...🙁");
+	throw new Error("Unable to connect to MongoDB Database");
+});
+
+process.on("SIGINT", () => {
+	logger.error("mongodb server: connection terminated...🟥");
+	db.close();
+});
+
+export default db;
