01e4726ca1d50c5ebf88c5c4c30f4a69215702c8
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-05-13 15:05:49 +0530

    feat(api): Updated env types

--- apps/api/env.d.ts
+++ apps/api/env.d.ts
@@ -1,5 +1,24 @@
 declare module NodeJS {
     interface ProcessEnv {
+        MONGODB_URL: string;
+        POSTGRES_URL: string;
+        REDIS_HOST: string;
+        REDIS_PORT: string;
+        REDIS_USERNAME: string;
+        REDIS_PASSWORD: string;
+        UPSTASH_REDIS_REST_URL: string;
+        UPSTASH_REDIS_REST_TOKEN: string;
+        CACHE_PROVIDER: string;
+        AWS_ACCESS_KEY: string;
+        AWS_SECRET_ACCESS_KEY: string;
+        AWS_BUCKET_NAME: string;
+        AWS_BUCKET_REGION: string;
+        AWS_BUCKET_IMAGE_URL_EXPIRATION: string;
+        ACCESS_TOKEN_SECRET: string;
+        REFRESH_TOKEN_SECRET: string;
+        ACCESS_TOKEN_VALIDITY: string;
+        REFRESH_TOKEN_VALIDITY: string;
+        REFRESH_TOKEN_MAX_AGE: string;
         FRONTEND_DEV_URL: string;
         FRONTEND_PROD_URL: string;
         NODE_ENV: "development" | "production";
