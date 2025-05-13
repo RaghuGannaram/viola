4cb47b946c9add51141665ef0f36ef6a1189d346
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-05-13 15:04:35 +0530

    feat(api): Configured aws

---
+++ apps/api/src/configs/aws.config.ts
@@ -0,0 +1,23 @@
+import { S3Client, GetObjectCommand, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import logger from "@src/configs/logger.config";
+import { getAWSParams } from "@src/utils/env-info";
+
+const { accessKey, secretAccessKey, region, signedURLValidity } = getAWSParams();
+
+const s3ClientInstance = new S3Client({
+	region: region,
+	credentials: {
+		accessKeyId: accessKey,
+		secretAccessKey: secretAccessKey,
+	},
+});
+
+logger.info(`aws s3 client: instance created successfully... ☁️`);
+
+export default {
+	s3ClientInstance,
+	GetObjectCommand,
+	PutObjectCommand,
+	DeleteObjectCommand,
+	signedURLValidity,
+};
