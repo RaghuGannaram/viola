17e905c9065509b15d1e7d43b91281fe4c3034dd
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-05-13 15:11:39 +0530

    feat(api): Updated utils

--- apps/api/src/utils/index.ts
+++ apps/api/src/utils/index.ts
@@ -1,11 +1,19 @@
+import crypto from "crypto";
+
 type Data = Record<string, any>;
 
-export function hideSensitiveInfo<T extends Data>(data: T, ...sensitiveInfo: (keyof T)[]): Partial<T> {
-    let clone: Partial<T> = {};
-    for (let key of Object.keys(data) as (keyof T)[]) {
-        if (!sensitiveInfo.includes(key)) {
-            clone[key] = data[key];
-        }
-    }
-    return clone;
+function hideSensitiveInfo<T extends Data>(data: T, ...sensitiveInfo: (keyof T)[]): Partial<T> {
+	let clone: Partial<T> = {};
+	for (let key of Object.keys(data) as (keyof T)[]) {
+		if (!sensitiveInfo.includes(key)) {
+			clone[key] = data[key];
+		}
+	}
+	return clone;
+}
+
+export default hideSensitiveInfo;
+
+export function generateRandomName(length: number): string {
+	return crypto.randomBytes(length).toString("hex");
 }
