c5731186e78e5bfafa2fd07cc3bb3390b731227c
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-05-13 15:20:40 +0530

    feat(api): Updated App

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,21 +1,25 @@
-import express, { type Request, type Response } from "express";
-import cors from "cors";
-import helmet from "helmet";
-import cookieParser from "cookie-parser";
-
-import morganMiddleware from "@src/middlewares/morgan.middleware";
+import api_v1 from "@src/api/v1";
 import customErrorHandler from "@src/middlewares/custom-error-handler.middleware";
 import defaultMiddleware from "@src/middlewares/default.middleware";
-import api_v1 from "@src/api/v1";
+import morganMiddleware from "@src/middlewares/morgan.middleware";
 import { getAPIGatewayURL } from "@src/utils/env-info";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { type Request, type Response } from "express";
+import helmet from "helmet";
+
 import logger from "./configs/logger.config";
+
+import "@src/configs/session.config";
+import "@src/configs/mongo.config";
+import "@src/configs/postgres.config";
 
 const app: express.Application = express();
 
 const apiGatewayURL = getAPIGatewayURL();
 const corsOptions = {
-    origin: apiGatewayURL,
-    credentials: true,
+	origin: apiGatewayURL,
+	credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -26,8 +30,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/health-check", (_req: Request, res: Response) => {
-    logger.info("Health check");
-    res.status(200).json({ message: "OK" });
+	logger.info("Health check");
+	res.status(200).json({ message: "OK" });
 });
 
 app.use("/api/v1", api_v1);
