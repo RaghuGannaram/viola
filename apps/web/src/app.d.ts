4530aef5a0e7985add2c722ae15a654037e08bc0
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-05-18 16:20:47 +0530

    feat(web): Configured server hook

--- apps/web/src/app.d.ts
+++ apps/web/src/app.d.ts
@@ -1,9 +1,14 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+import { type IProfile } from "$lib/types";
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+            profile: IProfile | null;
+            accessToken: string | null;
+        }
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
---
+++ apps/web/src/hooks.server.ts
@@ -0,0 +1,51 @@
+// src/hooks.server.ts
+
+import { authService } from "$lib/services/http";
+import { setParsedCookieFromBackend } from "$lib/utils";
+import { isValidJwt } from "$lib/utils/jwt";
+
+export async function handle({ event, resolve }) {
+	if (event.url.pathname.startsWith("/.well-known/")) {
+		return new Response(null, { status: 204 });
+	}
+
+	const profile = event.cookies.get("profile");
+
+	if (profile) {
+		event.locals.profile = JSON.parse(profile);
+	}
+
+	const accessToken = event.cookies.get("accessToken");
+
+	if (accessToken && isValidJwt(accessToken)) {
+		event.locals.accessToken = accessToken;
+		return resolve(event);
+	}
+
+	const refreshToken = event.cookies.get("refreshToken");
+
+	if (refreshToken) {
+		try {
+			const session = await authService.refresh(refreshToken);
+
+			event.locals.accessToken = session.accessToken;
+
+			event.cookies.set("accessToken", session.accessToken, {
+				httpOnly: true,
+				secure: true,
+				sameSite: "Strict",
+				path: "/",
+				maxAge: 60 * 60 * 2,
+			});
+
+			const rawRefreshToken = session.cookies.find((c) => c.startsWith("refreshToken="));
+
+			if (rawRefreshToken) setParsedCookieFromBackend(rawRefreshToken, event.cookies);
+		} catch (err) {
+			event.cookies.delete("refreshToken", { path: "/" });
+			event.cookies.delete("accessToken", { path: "/" });
+		}
+	}
+
+	return resolve(event);
+}
