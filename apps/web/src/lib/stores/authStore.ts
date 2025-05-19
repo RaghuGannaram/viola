8246b8173448fbacc6b8d76097d2abca15f038cc
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-05-19 23:52:12 +0530

    feat(web): Updated authStore

--- apps/web/src/lib/stores/authStore.ts
+++ apps/web/src/lib/stores/authStore.ts
@@ -1,14 +1,6 @@
 import { writable } from "svelte/store";
-import { browser } from "$app/environment";
+import { localStore } from "$lib/stores/localStore";
 import type { IProfile } from "$lib/types";
 
 export const accessToken = writable<string | null>(null);
-export const profile = writable<IProfile | null>(null);
-
-if (browser) {
-	const storedProfile = localStorage.getItem("profile");
-
-	if (storedProfile) {
-		profile.set(JSON.parse(storedProfile));
-	}
-}
+export const profile = localStore<IProfile | null>("profile", null);
