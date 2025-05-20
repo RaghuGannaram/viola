d7b17f5b31b64a747b3d222c97f75b17203b51a5
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-05-20 21:07:47 +0530

    feat(web): Configured page level authorization

---
+++ apps/web/src/routes/upload/+page.server.ts
@@ -0,0 +1,8 @@
+import { AccessLevel } from "$lib/types/auth.types.js";
+import { enforceAccess } from "$lib/utils/auth.js";
+
+export const load = async (event) => {
+	const profile = event.locals.profile;
+
+	enforceAccess(profile, AccessLevel.PRIVATE);
+};
