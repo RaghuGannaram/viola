26223c2e92cd66a699fa89fb3ed02a2f0561d2b1
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-05-29 23:40:32 +0530

    feat(web): Updated page layout

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import "../app.css";
 	import Header from "$lib/components/shared/Header.svelte";
+	import SideBar from "$lib/components/shared/SideBar.svelte";
 	import Footer from "$lib/components/shared/Footer.svelte";
 	import { accessToken, profile } from "$lib/stores/authStore";
 
@@ -12,12 +13,16 @@
 	});
 </script>
 
-<div class="flex flex-col bg-neutral-900 text-neutral-100">
+<div class="flex flex-col min-h-screen bg-gradient-to-br from-neutral-950/50 to-neutral-900/50 text-surface-50">
 	<Header />
 
-	<main>
-		{@render children()}
-	</main>
+	<div class="flex flex-1 overflow-hidden">
+		<SideBar customCalss="w-[100%] md:w-[80px]" />
+
+		<main class="w-[100%] md:w-[80%] overflow-y-auto">
+			{@render children()}
+		</main>
+	</div>
 
 	<Footer />
 </div>
