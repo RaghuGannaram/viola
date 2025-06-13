76736ef854079d397abafe8c661d60732d342e10
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-06-13 21:24:52 +0530

    feat(api): Updated AWS data service

--- apps/api/src/services/aws.data.ts
+++ apps/api/src/services/aws.data.ts
@@ -1,25 +1,24 @@
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import AWS from "@src/configs/aws.config";
 import logger from "@src/configs/logger.config";
+import { AssetCategory } from "@src/types";
 import { generateRandomName } from "@src/utils";
 import { catchAsyncDataError, processAWSError } from "@src/utils/application-errors";
-import { getAWSParams } from "@src/utils/env-info";
 import ms from "ms";
-
-const { bucket } = getAWSParams();
+import { v4 as uuid } from "uuid";
 
 const getFile = catchAsyncDataError(async (key: string): Promise<string> => {
 	logger.debug(`aws.service: getting signed URL for file: %s from s3 bucket`, key);
 
 	let fileURL = "";
 	const params = {
-		Bucket: bucket,
+		Bucket: AWS.bucket,
 		Key: key,
 	};
 
 	try {
 		fileURL = await getSignedUrl(AWS.s3ClientInstance, new AWS.GetObjectCommand(params), {
-			expiresIn: ms(AWS.signedURLValidity) / 1000,
+			expiresIn: ms(AWS.signedUrlValidity) / 1000,
 		});
 	} catch (error) {
 		processAWSError(error);
@@ -28,12 +27,35 @@ const getFile = catchAsyncDataError(async (key: string): Promise<string> => {
 	return fileURL;
 });
 
+const presignPutUrl = catchAsyncDataError(async (category: AssetCategory, fileName: string, contentType: string): Promise<{ presignedUrl: string; s3Key: string }> => {
+	const s3Key = `${category}/${uuid()}-${fileName}`;
+
+	logger.debug(`aws.service: generating presigned PUT URL for key: %s`, s3Key);
+
+	const params = {
+		Bucket: AWS.bucket,
+		Key: s3Key,
+		ContentType: contentType,
+	};
+
+	let presignedUrl = "";
+	try {
+		presignedUrl = await getSignedUrl(AWS.s3ClientInstance, new AWS.PutObjectCommand(params), {
+			expiresIn: ms(AWS.signedUrlValidity) / 1000,
+		});
+	} catch (error) {
+		processAWSError(error);
+	}
+
+	return { presignedUrl, s3Key };
+});
+
 const uploadFile = catchAsyncDataError(async (imageCategory: string, imageBuffer: Buffer): Promise<string> => {
 	logger.debug(`aws.service: uploading %s file to s3 bucket`, imageCategory);
 
 	const uniqueFileName = imageCategory + "_" + generateRandomName(16);
 	const params = {
-		Bucket: bucket,
+		Bucket: AWS.bucket,
 		Key: uniqueFileName,
 		Body: imageBuffer,
 		ContentType: "image/webp",
@@ -52,7 +74,7 @@ const deleteFile = catchAsyncDataError(async (key: string): Promise<void> => {
 	logger.debug(`aws.service: deleting file: %s from s3 bucket`, key);
 
 	const params = {
-		Bucket: bucket,
+		Bucket: AWS.bucket,
 		Key: key,
 	};
 
@@ -65,6 +87,7 @@ const deleteFile = catchAsyncDataError(async (key: string): Promise<void> => {
 
 export default {
 	getFile,
+	presignPutUrl,
 	uploadFile,
 	deleteFile,
 };
