169d6fa518c4ed38ad32c6edbbfa2c538af8f2ba
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-06-14 14:52:27 +0530

    feat(api): Created and refactored constants

---
+++ apps/api/src/constants/image.constants.ts
@@ -0,0 +1,8 @@
+const imageSize = {
+	AVATAR_SIZE: 480,
+	BACKGROUND_SIZE: 1080,
+	POST_IMAGE_THUMBNAIL_SIZE: 50,
+	POST_IMAGE_ORIGINAL_SIZE: 1080,
+};
+
+export default { imageSize };
---
+++ apps/api/src/constants/index.ts
@@ -0,0 +1,4 @@
+import AUDIO_CONSTANTS from "./audio.constants";
+import IMAGE_CONSTANTS from "./image.constants";
+
+export { IMAGE_CONSTANTS, AUDIO_CONSTANTS };
