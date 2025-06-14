0a9b8a6b5b8b393a711ed8d782433f790dd18d3c
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-06-14 14:47:04 +0530

    feat(api): Updaated main routes

--- apps/api/src/api/v1/index.ts
+++ apps/api/src/api/v1/index.ts
@@ -1,6 +1,13 @@
+import audioRouter from "@src/routes/audio.route";
+import authRouter from "@src/routes/auth.route";
 import express from "express";
 import type { Router } from "express";
 
+
 const router: Router = express.Router();
+
+router.use("/auth", authRouter);
+
+router.use("/audio", audioRouter);
 
 export default router;
