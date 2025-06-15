ce77f70c699969028c090a3e7152e82b0a1e8549
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-06-15 21:28:59 +0530

    feat(web): Created home page

---
+++ apps/web/src/routes/home/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { trackMetadataList } from "$lib/stores/trackMetadataStore";
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		trackMetadataList.hydrate();
+	});
+</script>
+
+<section>
+	<h1>Track Metadata</h1>
+	<p>Here are the tracks loaded from the store:</p>
+	{#each $trackMetadataList as track}
+		<div><img src={track.artworkUrl} width="50px" height="50px" alt={track.title} /> <span>{track.title}</span></div>
+	{/each}
+</section>
