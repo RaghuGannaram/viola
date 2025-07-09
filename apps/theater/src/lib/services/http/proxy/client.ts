dbcf9103b014867d7a90cdf4f749dabcc1c1fc42
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-07-09 22:43:51 +0530

    feat(theater): Updated proxyClient config

--- apps/theater/src/lib/services/http/proxy/client.ts
+++ apps/theater/src/lib/services/http/proxy/client.ts
@@ -5,6 +5,7 @@ import { PUBLIC_API_PROXY_BASE } from "$env/static/public";
 const proxyClient = axios.create({
 	baseURL: PUBLIC_API_PROXY_BASE,
 	withCredentials: true,
+	validateStatus: (status) => status < 500,
 });
 
 setupInterceptors(proxyClient);
