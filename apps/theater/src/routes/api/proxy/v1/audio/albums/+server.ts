d117c30ffd86030ac7b615a74c98b129f4cebd3f
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-07-09 22:47:03 +0530

    feat(theater): Created tracks, albums and artists proxy routes

---
+++ apps/theater/src/routes/api/proxy/v1/audio/albums/+server.ts
@@ -0,0 +1,20 @@
+import type { RequestHandler } from "@sveltejs/kit";
+import { createProxyService } from "$lib/services/http/proxy.service";
+import { BACKEND_ENDPOINTS } from "$lib/services/http/shared/endpoints";
+
+export const GET: RequestHandler = async (event) => {
+	const proxy = createProxyService(event);
+
+	const { status, body, cookies } = await proxy.forward("GET", BACKEND_ENDPOINTS.AUDIO.LIST_ALBUMS);
+
+	const headers = new Headers();
+
+	for (const cookie of cookies) {
+		headers.append("set-cookie", cookie.replace("Path=/api/v1/audio/", "Path=/"));
+	}
+
+	return new Response(JSON.stringify(body), {
+		status,
+		headers,
+	});
+};
---
+++ apps/theater/src/routes/api/proxy/v1/audio/artists/[id]/+server.ts
@@ -0,0 +1,22 @@
+import type { RequestHandler } from "@sveltejs/kit";
+import { createProxyService } from "$lib/services/http/proxy.service";
+import { BACKEND_ENDPOINTS } from "$lib/services/http/shared/endpoints";
+
+export const GET: RequestHandler = async (event) => {
+	const proxy = createProxyService(event);
+
+	const { status, body, cookies } = await proxy.forward("GET", `${BACKEND_ENDPOINTS.AUDIO.SHOW_ARTIST.replace(":id", event.params["id"] ?? "")}`);
+
+	const headers = new Headers();
+
+	for (const cookie of cookies) {
+		headers.append("set-cookie", cookie.replace("Path=/api/v1/audio/", "Path=/"));
+	}
+
+	console.log(status, body);
+
+	return new Response(JSON.stringify(body), {
+		status,
+		headers,
+	});
+};
